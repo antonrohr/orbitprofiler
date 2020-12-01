632186a3f782748d2fa8a6d73e8af9a9142fed82
antonrohr <antonrohr@google.com>
2020-12-01 17:09:23 +0100

    Add nodiscard to InstanceItemModel

--- OrbitGgp/include/OrbitGgp/InstanceItemModel.h
+++ OrbitGgp/include/OrbitGgp/InstanceItemModel.h
@@ -23,15 +23,15 @@ class InstanceItemModel : public QAbstractItemModel {
 
   void SetInstances(QVector<Instance> instances);
 
-  int columnCount(const QModelIndex& parent = {}) const override;
-  QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
-  QVariant headerData(int section, Qt::Orientation orientation,
-                      int role = Qt::DisplayRole) const override;
-  QModelIndex index(int row, int col, const QModelIndex& parent = {}) const override;
-  QModelIndex parent(const QModelIndex& parent) const override;
-  int rowCount(const QModelIndex& parent = {}) const override;
+  [[nodiscard]] int columnCount(const QModelIndex& parent = {}) const override;
+  [[nodiscard]] QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+  [[nodiscard]] QVariant headerData(int section, Qt::Orientation orientation,
+                                    int role = Qt::DisplayRole) const override;
+  [[nodiscard]] QModelIndex index(int row, int col, const QModelIndex& parent = {}) const override;
+  [[nodiscard]] QModelIndex parent(const QModelIndex& parent) const override;
+  [[nodiscard]] int rowCount(const QModelIndex& parent = {}) const override;
 
-  std::optional<int> GetRowOfInstanceById(const QString& instance_id);
+  [[nodiscard]] std::optional<int> GetRowOfInstanceById(const QString& instance_id);
 
  private:
   QVector<Instance> instances_;
